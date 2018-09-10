80a6413d58fb56ee0fee1f7e976de53be7e528a1
sithum2 <monchat@gmail.com>
2018-09-09 23:07:04 -0400

    first commit

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+int main(int argc, char **argv)
+{	
+	vector<int> the_list = {32,12,10,13,41,10,11,10,10,11,13,14,10};
+
+	int item = 10;
+
+	int num = count(the_list.cbegin(), the_list.cend(), item);
+
+	cout << "there are "     << num
+	     << " instances of " << item
+	     << endl;
+
+	return 0;
+}
