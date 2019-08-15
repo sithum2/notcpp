24d9cd77b53933dabe57730ee179786aafc3884d
David Charles <davidcharles2@Daves-MacBook-Pro.local>
2019-08-15 17:26:43 -0400

    fixed if statement. removed auto from if statement which seemed to cause an error

--- ex953.cpp
+++ ex953.cpp
@@ -15,15 +15,15 @@ Iter find_it(Iter beg, Iter end, const Value &v)
 
 int main(int argc, char **argv)
 {
-
 	vector<int> nums{4,5,6,7,8,4,3,4,56,7};
 
 	int n = 3;
 
-	if(auto i = find_it(nums.begin(), nums.end(), n) != nums.end())
+	auto i = find_it(nums.begin(), nums.end(), n);
+
+	if(i != nums.end())
 	{
-		cout <<  n << " found " << " at index " << i << endl;
-		cout << nums[i] << endl;
+		cout <<  n << " found " << " at index " << i - nums.begin() << endl;
 	}else
 	{
 		cout << "number not found" << endl;
