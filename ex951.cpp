8acd616be501d54f07e8c8fe9b6bac2d620d9553
David Charles <davidcharles@David-Dicks-Mac-mini.local>
2019-08-15 14:34:36 -0400

    added ex951.cpp ranged for loops and normal for loops

--- ex951.cpp
+++ ex951.cpp
@@ -14,11 +14,23 @@ T accum(vector<T> &v)
 	return sum;
 }
 
+template
+<typename T>
+T sum(vector<T> &v)
+{
+	T sum = 0;
+	for(auto i = v.begin(); i != v.end(); ++i)
+		sum += *i;
+
+	return sum;
+}
+
 int main(int argc, char **argv)
 {
 	vector<int> nums{4,3,5,6,7};
 
 	cout << accum(nums) << endl;
+	cout << sum(nums)   << endl;
 
 	return 0;
 }
